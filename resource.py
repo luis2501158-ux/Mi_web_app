75d2be483943c957df397c47f21b4c64c54863e9
luis2501158-ux <luis2501158@hybridge.education>
2026-01-04 20:54:20 -0600

    Se cambió la ruta de la página principal

--- resource.py
+++ resource.py
@@ -2,13 +2,14 @@ from flask_restful import Resource
 
 from flask import make_response, render_template
 
+class pantalla_inicio(Resource):
+    def get(self):
+        contenido = render_template('index.html')
+        return make_response(contenido)
+
 #Vamos a crear recursos
 class HelloWorld(Resource):
     def get(self):
 
-        contenido = render_template('index.html')
-        return make_response(contenido)
+        return "Esta es la pantalla inicio"
 
-class pantalla_inicio(Resource):
-    def get(self):
-        return 'Esta es la pantalla inicio'
